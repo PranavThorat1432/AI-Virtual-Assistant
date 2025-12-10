83f16dbe36e1331c606250a7007caa9f13f87f68
PranavThorat1432 <pranavthorat95@gmail.com>
2025-12-10 18:04:33 +0530

    AuthController

---
+++ Server/controllers/authController.js
@@ -0,0 +1,113 @@
+import genToken from "../config/token.js";
+import User from "../models/UserModel.js";
+import bcrypt from 'bcryptjs';
+
+
+export const signUp = async (req, res) => {
+    try {
+        const {name, email, password} = req.body;
+
+        const existEmail = await User.findOne({email});
+        if(existEmail) {
+            return res.status(400).json({
+                success: true,
+                message: 'Email already exist!'
+            });
+        }
+
+        if(password.length < 6) {
+            return res.status(400).json({
+                success: true,
+                message: 'Password must be atleast of 6 characters!'
+            });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        
+        const user = await User.create({
+            name, email, password: hashedPassword
+        });
+    
+        const token = await genToken(user._id);
+
+        res.cookie('token', token, {
+            httpOnly: true,
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+            sameSite: 'strict',
+            secure: false
+        });
+
+        return res.status(201).json({
+            success: true,
+            message: 'User Created Successfully!',
+            user
+        });
+
+
+    } catch (error) {
+        return res.status(500).json({
+            message: `SignUp Error: ${error}`
+        });
+    }
+};
+
+
+export const login = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+
+        const user = await User.findOne({email});
+        if(!user) {
+            return res.status(400).json({
+                success: true,
+                message: 'Email does not exist!'
+            });
+        }
+
+        const isMatched = await bcrypt.compare(password, user.password);
+        if(!isMatched) {
+            return res.status(400).json({
+                success: true,
+                message: 'Incorrect Password!'
+            });
+        }
+    
+        const token = await genToken(user._id);
+
+        res.cookie('token', token, {
+            httpOnly: true,
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+            sameSite: 'strict',
+            secure: false
+        });
+
+        return res.status(200).json({
+            success: true,
+            message: 'User Logged In Successfully!',
+            user
+        });
+
+
+    } catch (error) {
+        return res.status(500).json({
+            message: `Login Error: ${error}`
+        });
+    }
+};
+
+
+export const logout = async (req, res) => {
+    try {
+        res.clearCookie('token');
+
+        return res.status(200).json({
+            success: true,
+            message: 'User Logged Out Successfully!',
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            message: `Logout Error: ${error}`
+        });
+    }
+};
